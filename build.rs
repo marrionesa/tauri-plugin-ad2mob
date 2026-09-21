6d045ce96a637a47805f2f6a5013a2a1cb76b668
marrionesa <fitypaldy@gmail.com>
2026-09-21 19:44:49 +0200

    fix: integrate iOS framework support and private cargo dependencies

--- build.rs
+++ build.rs
@@ -32,6 +32,10 @@ fn main() {
     let result = tauri_plugin::Builder::new(COMMANDS)
         .android_path("android")
         .ios_path("ios")
+        .ios_frameworks([
+            "GoogleMobileAds",
+            "GoogleUserMessagingPlatform",
+        ])
         .try_build();
 
     // when building documentation for Android the plugin build result is always Err() and is irrelevant to the crate documentation build
